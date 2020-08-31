3c54fbbfd7fbbca3a3804f1c824ceb2c857a1c21
Abhishek Singh <abhishek3571@gmail.com>
2020-08-31 23:43:14 +0530

    Observing Events and applying operators in Rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,44 @@
 import "./index.css";
 
-import { Observable } from 'rxjs';
+import { fromEvent } from "rxjs";
 
-let nums = [2, 4, 6, 9, 10];
+import { pluck, timeInterval, map } from "rxjs/operators";
+
+
+
+let clicks$ = fromEvent(document, "click");
+
+clicks$.pipe(
+    pluck('clientX'),
+    timeInterval(),
+    map(clickInfo => `${clickInfo.interval / 1000} seconds (${clickInfo.value})`)
+)
+.subscribe(
+  (value: any) => console.log(value),
+  (err: any) => console.log(`ERROR: ${err}`),
+  () => console.log("All done.")
+);
+
+//let nums = [2, 4, 6, 9, 10];
 
 //let numsObservable$ = from (nums);
 
-let evenNumbers$ = Observable.create((subscriber: { next: (arg0: number) => void; error: (arg0: string) => void; complete: () => void; }) => {
-    
-    for (let currentNum of nums) {
-        if (currentNum % 2 === 0) {
-            subscriber.next(currentNum);
-        } else {
-            subscriber.error('Value is not even');
-            
-        }
-    }
+// let evenNumbers$ = Observable.create((subscriber: { next: (arg0: number) => void; error: (arg0: string) => void; complete: () => void; }) => {
 
-    subscriber.complete();
-})
+//     for (let currentNum of nums) {
+//         if (currentNum % 2 === 0) {
+//             subscriber.next(currentNum);
+//         } else {
+//             subscriber.error('Value is not even');
 
+//         }
+//     }
 
+//     subscriber.complete();
+// })
 
-evenNumbers$.subscribe(
-    (value: any) => console.log(value),
-    (err: any) => console.log(`ERROR: ${err}`),
-    () => console.log('All done.'),
-);
-
-
+// evenNumbers$.subscribe(
+//     (value: any) => console.log(value),
+//     (err: any) => console.log(`ERROR: ${err}`),
+//     () => console.log('All done.'),
+// );
